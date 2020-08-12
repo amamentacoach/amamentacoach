cb3840d56caa8d57e6224faa3ab8bb5d60acbeaa
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2020-08-12 15:39:41 -0300

    create, show -> Ordenha

---
+++ backend/src/controllers/OrdenhasController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class OrdenhasController{
+    async show(req:Request, res:Response){
+        const {mae_id} = req.params;
+
+        const {nome} = await knex('mae').select('nome').first()
+
+        const ordenhas = await knex('ordenha')
+            .select('*')
+            .where('mae_id',mae_id).orderBy('data_hora','desc')
+
+        return res.json({
+            id:parseInt(mae_id),
+            nome,
+            ordenhas
+        })
+    }
+
+    async create(req:Request,res:Response){
+        const {
+            qtd_leite,
+            mama,
+            duracao,
+        } = req.body;
+
+        const {mae_id} = req.params;
+
+        const ordenha = {mae_id:parseInt(mae_id), qtd_leite, mama, duracao, data_hora:new Date}
+        
+        await knex('ordenha').insert(ordenha);
+
+        res.sendStatus(200);
+    }
+}
+
+export default OrdenhasController;
