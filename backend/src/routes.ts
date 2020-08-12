a6e22c5e25acc5121fcac064c351d134eb6c8276
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2020-08-12 15:38:46 -0300

    novas rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,25 @@
 import {Router, Request, Response} from 'express';
+import MaesController from './controllers/MaesController';
+import OrdenhasController from './controllers/OrdenhasController';
+import MensagensController from './controllers/MensagensController';
+
+
+const maesController = new MaesController();
+const ordenhasController = new OrdenhasController();
+const mensagensController = new MensagensController();
 
 const routes = Router()
 
-routes.get('/',(req : Request,res:Response)=>res.send('Hello World'))
+routes.get('/',(req : Request,res:Response)=>res.send('Hello World'));
 
+routes.post('/maes',maesController.create);
+routes.get('/maes', maesController.index);
+routes.get('/maes/:id', maesController.show);
+
+routes.post('/maes/:mae_id/ordenhas',ordenhasController.create);
+routes.get('/maes/:mae_id/ordenhas',ordenhasController.show);
+
+routes.post('/mensagens',mensagensController.create);
+routes.get('/mensagens',mensagensController.index);
 
 export default routes;
