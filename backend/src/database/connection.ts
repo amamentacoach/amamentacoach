8299962c1e93f30e5326fd76ddcee5873b2b3b95
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2020-08-13 14:04:19 -0300

    ajustes

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -2,7 +2,7 @@ import knex from 'knex';
 
 const connection = knex({
     client:'pg',
-    connection:{
+    connection:process.env.DATABASE_URL||{
         host:'localhost',
         user:'joao',
         password:'joao',
