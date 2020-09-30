c1bbd0c8e3df6c4102487a5c8a0c761e021f2b5f
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2020-09-30 14:01:59 -0300

    atualizando documentacao da api

--- backend/public/apidoc/api_project.js
+++ backend/public/apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-24T18:18:43.687Z",
+    "time": "2020-09-30T17:01:45.657Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
