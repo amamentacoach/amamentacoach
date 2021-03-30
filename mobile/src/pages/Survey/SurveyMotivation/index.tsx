1848fd77e4b6ef2c7af14924650a40b0c29eda3e
Olavo Nascimento <olavo.nascimento4@gmail.com>
2021-03-30 16:57:59 -0300

    Adicionar enquete Motivação.
    Inclui a enquete Motivação no menu de enquetes.
    
    Closes #66.

---
+++ mobile/src/pages/Survey/SurveyMotivation/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import DiaryForm from '../../../components/DiaryForm';
+import createGenericDiaryFormPage from '../../../components/GenericDiaryFormPage';
+
+const SurveyMotivation: React.FC = () => {
+  const navigation = useNavigation();
+  const onFormEnd = () => navigation.navigate('Survey');
+
+  return (
+    <DiaryForm
+      title="Motivação"
+      category={8}
+      InfoPage={createGenericDiaryFormPage(onFormEnd)}
+    />
+  );
+};
+
+export default SurveyMotivation;
