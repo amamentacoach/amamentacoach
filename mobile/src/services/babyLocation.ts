0be3c78a1b97cfce8a0c8ee7af1bcc137bc6e4ad
Olavo Nascimento <olavo.nascimento4@gmail.com>
2021-03-30 10:52:52 -0300

    Implementar popup de localização do bebê.
    Ao abrir o aplicativo pela primeira vez no dia é exibido um popup
    perguntado a localização de cada bebê que pode receber alta.
    
    Closes #65

---
+++ mobile/src/services/babyLocation.ts
@@ -0,0 +1,26 @@
+import api from './api';
+
+export interface IBabyLocation {
+  id: string;
+  name: string;
+  location: 'alojamento conjunto' | 'uci neonatal' | 'uti neonatal';
+  date: string;
+}
+
+// Lista os bebês que podem receber alta.
+export async function checkBabiesLocation(): Promise<IBabyLocation[] | null> {
+  try {
+    const { data } = await api.get('/bebes/alta');
+    const babies = data.map((baby: any) => ({
+      id: baby.id.toString(),
+      name: baby.nome,
+      location: baby.local.toLowerCase(),
+      date: baby.data_parto,
+    }));
+    return babies;
+  } catch (error) {
+    return null;
+  }
+}
+
+export default checkBabiesLocation;
