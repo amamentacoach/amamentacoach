27734e227add045cc0a2b3ad0930cdc0531bb2e3
Joao Alex <joao.alex.bergamo@uel.br>
2021-04-27 16:24:51 -0300

    Notificacoes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,12 +8,12 @@ import sendPushNotification from './utils/sendPushNotification';
 
 const app = express()
 
-const rule = new schedule.RecurrenceRule();
-rule.tz = 'America/Sao_Paulo'
-rule.dayOfWeek = [0, new schedule.Range(0,6)]
-rule.hour = 21;
-rule.minute = 0;
-schedule.scheduleJob(rule,sendPushNotification)
+//const rule = new schedule.RecurrenceRule();
+//rule.tz = 'America/Sao_Paulo'
+//rule.dayOfWeek = [0, new schedule.Range(0,6)]
+//rule.hour = 21;
+//rule.minute = 0;
+//schedule.scheduleJob(rule,sendPushNotification)
 
 app.set('view engine','ejs');
 app.use("/",express.static(path.resolve(__dirname, '..', 'public','apidoc')));
