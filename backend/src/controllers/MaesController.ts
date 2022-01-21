55336952779a40daa6ec256567cbcacb0b04f20e
Joao Alex <joao.alex.bergamo@uel.br>
2022-01-21 10:53:17 -0300

    Adição dos campos nas rotas de dados das maes e dos bebes #103

--- backend/src/controllers/MaesController.ts
+++ backend/src/controllers/MaesController.ts
@@ -12,25 +12,22 @@ class MaesController{
         if(id==req.mae_id){
 
             const mae = await knex('mae')
-            .select('mae.id', 
-                    'email', 
-                    'mae.nome', 
-                    'ultimo_acesso', 
-                    'imagem_mae', 
-                    'imagem_bebe', 
-                    'imagem_pai', 
-                    'companheiro', 
-                    'data_nascimento', 
-                    'localizacao')
+            .select('*')
             .where('mae.id',id).first()
+
+            mae['senha'] = null;
+
+            mae['tempo_amamentacao'] = mae['tempo_amamentacao'].split('|')
 
 
             const bebes = await knex('bebe').select('*').where('mae_id',id)
             const ordenhas = await knex('ordenha').select('*').where('mae_id',id)
 
-            for (let index = 0; index < bebes.length; index++)
+            for (let index = 0; index < bebes.length; index++){
+                bebes[index].primeiro_estimulo = bebes[index].primeiro_estimulo.split('|')
                 bebes[index].mamadas = await knex('mamada').select('id','data_hora','mama','duracao').where('bebe_id','=',`${bebes[index].id}`)
-                
+            }
+               
             mae.bebes=bebes
             mae.ordenhas=ordenhas
             return res.json(mae);
