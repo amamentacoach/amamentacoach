c4beda32c08304e1d299441cd1609b86401a7370
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-06-30 23:15:45 -0300

    Simplificar estilos.

--- mobile/src/pages/Home/MusicPlaylists/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-
-export default styled.ScrollView.attrs(() => ({
-  contentContainerStyle: { flexGrow: 1 },
-  keyboardShouldPersistTaps: 'handled',
-}))`
-  flex: 1;
-  padding: 24px;
-`;
